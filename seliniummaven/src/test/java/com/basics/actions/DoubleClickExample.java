8012944cc36d70defcec6459abf69e8bbe4e562d
Avinash <avinash.dj@gmail.com>
2021-03-26 00:10:59 +0530

    Action class pushed

---
+++ seliniummaven/src/test/java/com/basics/actions/DoubleClickExample.java
@@ -0,0 +1,89 @@
+package com.basics.actions;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DoubleClickExample {
+	WebDriver driver;
+	
+	@BeforeClass
+	public void setUp() {
+		// Get the project root directory using user.dir system property
+		String userDir = System.getProperty("user.dir");
+		// setting Chrome driver executable path or use WebDriverManager class
+		// System.setProperty("webdriver.chrome.driver",
+		// userDir+"/src/test/resources/drivers/chromedriver.exe");
+		WebDriverManager.chromedriver().setup();
+		// Initializing WebDriver
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+	}
+
+	@Test
+	public void doubleClick() throws InterruptedException {
+		
+		driver.get("http://djangovinoth.pythonanywhere.com/login/");
+
+		Actions action = new Actions(driver);
+		
+		WebElement login = driver.findElement(By.name("username"));
+		login.sendKeys("avinashdj");
+		
+		WebElement password = driver.findElement(By.name("password"));
+		password.sendKeys("Sadu2006$");
+		
+		WebElement submit = driver.findElement(By.xpath("//button[@type='submit']"));
+		submit.click();
+		
+		WebElement skillEnhancmentMenu = driver.findElement(By.id("navbarDropdownMenuLink"));
+		skillEnhancmentMenu.click();
+		
+		WebElement labPractice = driver.findElement(By.linkText("Lab Practice"));
+		labPractice.click();
+		
+		Thread.sleep(5000);
+		
+		WebElement actionsLink = driver.findElement(By.partialLinkText("Actions"));
+		actionsLink.click();
+		
+		Thread.sleep(5000);
+		
+		WebElement doubleClick = driver.findElement(By.partialLinkText("Double Click"));
+		doubleClick.click();
+		
+		WebElement dblClickButton = driver.findElement(By.id("dbclick1"));
+		
+		action.doubleClick(dblClickButton).build().perform();
+		
+		Thread.sleep(5000);
+		
+		WebElement dblClickImage = driver.findElement(By.id("dbclick2"));
+		
+		action.doubleClick(dblClickImage).build().perform();
+		
+		Thread.sleep(5000);
+
+	}
+
+	@AfterClass
+	public void tearDown() {
+		driver.close();
+		driver.quit();
+	}
+}
---
+++ seliniummaven/src/test/java/com/basics/actions/SliderExample.java
@@ -0,0 +1,82 @@
+package com.basics.actions;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SliderExample {
+	WebDriver driver;
+	
+	@BeforeClass
+	public void setUp() {
+		// Get the project root directory using user.dir system property
+		String userDir = System.getProperty("user.dir");
+		// setting Chrome driver executable path or use WebDriverManager class
+		// System.setProperty("webdriver.chrome.driver",
+		// userDir+"/src/test/resources/drivers/chromedriver.exe");
+		WebDriverManager.chromedriver().setup();
+		// Initializing WebDriver
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+	}
+
+	@Test
+	public void sliderExample() throws InterruptedException {
+		
+		driver.get("http://djangovinoth.pythonanywhere.com/login/");
+		
+		WebElement login = driver.findElement(By.name("username"));
+		login.sendKeys("avinashdj");
+		
+		WebElement password = driver.findElement(By.name("password"));
+		password.sendKeys("Sadu2006$");
+		
+		WebElement submit = driver.findElement(By.xpath("//button[@type='submit']"));
+		submit.click();
+		
+		WebElement skillEnhancmentMenu = driver.findElement(By.id("navbarDropdownMenuLink"));
+		skillEnhancmentMenu.click();
+		
+		WebElement labPractice = driver.findElement(By.linkText("Lab Practice"));
+		labPractice.click();
+		
+		Thread.sleep(5000);
+		
+		WebElement actionsLink = driver.findElement(By.partialLinkText("Actions"));
+		actionsLink.click();
+		
+		WebElement  mouseHover = driver.findElement(By.partialLinkText("Mouse Hover"));
+		mouseHover.click();
+		
+		WebElement portfolio = driver.findElement(By.xpath("//a[@id = 'id6']"));
+
+		Actions action = new Actions(driver);
+		
+		action.moveToElement(portfolio).build().perform();
+
+		Thread.sleep(5000);
+
+	}
+
+	@AfterClass
+	public void tearDown() {
+		driver.close();
+		driver.quit();
+	}
+}
