0a538a905826714a75a9f9e72a44c2554f263bad
Avinash <avinash.dj@gmail.com>
2021-04-25 09:56:15 +0530

    Minor fix to the page provider class

--- seliniummaven/src/test/java/edu/framework/factory/PageProvider.java
+++ seliniummaven/src/test/java/edu/framework/factory/PageProvider.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
 import edu.framework.pageobject.HomePage;
-import edu.framework.pageobject.HomePageB165;
 import edu.framework.pageobject.LoginPage;
 
 public class PageProvider {
